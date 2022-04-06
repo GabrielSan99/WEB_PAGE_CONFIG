41fbf2fda5dd2281332bd29baa3eaf4d8b166af4
GabrielSan99 <gabriel_j.sanches@hotmail.com>
2022-04-06 16:59:07 -0300

    home_almost_finished

--- change_ini.py
+++ change_ini.py
@@ -1,12 +1,21 @@
 import configparser
+import requests
 
-cfg= configparser.ConfigParser()
+cfg = configparser.ConfigParser()
 cfg.read('config_test.ini')
 
-x = cfg['general']['api_key']
-print(x)
+r = requests.get('http://192.168.0.14:82/get_config')
+r = r.json()
+print(r.json())
 
-cfg['general']['api_key'] = 'nova chave'            #update value
 
-with open('config_test.ini', 'w') as configfile:    # save
-    cfg.write(configfile)
+
+
+
+# cfg['general']['api_key'] = 'nova chave'            #update value
+
+# with open('config_test.ini', 'w') as configfile:    # save changes
+#     cfg.write(configfile)
+
+# x = cfg['general']['api_key']                       #read value
+# print(x)
